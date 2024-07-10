9f03bc2a789fd2021a059a9a69c3fb9e504ede37
rolando1601 <rolando.irribarra1601@alumnos.ubiobio.cl>
2024-07-10 03:17:56 -0400

    hola mundo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1>Welcome to Next.js!</h1>
+        <h1>hola mundo</h1>
         <Image src="/images/vercel.png" alt="Vercel Logo" width={72} height={16} />
       </div>
     </main>
