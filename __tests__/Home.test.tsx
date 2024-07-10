0347f99f6bb623565085582f2eea4f81f530a0e3
rolando1601 <rolando.irribarra1601@alumnos.ubiobio.cl>
2024-07-10 03:21:30 -0400

    commit 10

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -3,9 +3,11 @@ import { render, screen } from '@testing-library/react';
 import Home from '@/app/page';
 
 describe('Home Page', () => {
-  it('renders welcome message', () => {
+  it('renders some text', () => {
     render(<Home />);
-    const heading = screen.getByText(/Welcome to Next.js!/i);
+    const heading = screen.getByText((content, element) => {
+      return element.tagName.toLowerCase() === 'h1' && !!content.trim();
+    });
     expect(heading).toBeInTheDocument();
   });
 
